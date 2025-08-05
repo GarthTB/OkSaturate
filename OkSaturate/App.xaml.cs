7bf08fdc76d697cfb60d8d8ea7ed29f5d5cc2e1d
Garth天卜 <g-art-h@outlook.com>
2025-08-05 14:34:03 +0800

    清理初始代码

--- OkSaturate/App.xaml.cs
+++ OkSaturate/App.xaml.cs
@@ -1,14 +1,3 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+namespace OkSaturate;
 
-namespace OkSaturate
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
-
-}
+public partial class App : System.Windows.Application;
---
+++ OkSaturate/Views/MainWindow.xaml.cs
@@ -0,0 +1,4 @@
+namespace OkSaturate.Views;
+
+public partial class MainWindow : System.Windows.Window
+{ public MainWindow() => InitializeComponent(); }
