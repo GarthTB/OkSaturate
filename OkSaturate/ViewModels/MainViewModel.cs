d9dfddbb3d95243b4b58b4f1e6df8f40e408e1c0
Garth天卜 <g-art-h@outlook.com>
2025-08-06 17:52:28 +0800

    界面初步设计完成

---
+++ OkSaturate/ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace OkSaturate.ViewModels;
+
+/// <summary> MainWindow.xaml 的视图模型 </summary>
+internal partial class MainViewModel : ObservableObject
+{
+
+}
