a38f11c2badd1946749e5a9dd6a6a7ec777f6b27
Garth天卜 <g-art-h@outlook.com>
2025-08-06 18:06:01 +0800

    实现添加和移除待处理图像

---
+++ OkSaturate/Utils/Try.cs
@@ -0,0 +1,20 @@
+namespace OkSaturate.Utils;
+
+/// <summary> 用于简化Try-Catch块的工具类 </summary>
+internal static class Try
+{
+    /// <summary> 执行一个Action，若有异常则弹窗提示 </summary>
+    public static void Do(string actionName, Action action)
+    {
+        try { action(); }
+        catch (Exception ex)
+        {
+            var msg = string.IsNullOrWhiteSpace(ex.Message)
+                ? "无详细错误信息"
+                : string.IsNullOrWhiteSpace(ex.StackTrace)
+                ? ex.Message
+                : $"{ex.Message}\n\n栈跟踪：\n\n{ex.StackTrace}";
+            MsgBox.Error($"{actionName} 出错：\n{msg}");
+        }
+    }
+}
--- OkSaturate/ViewModels/MainViewModel.cs
+++ OkSaturate/ViewModels/MainViewModel.cs
@@ -1,9 +1,54 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
+using OkSaturate.Utils;
+using System.Collections.ObjectModel;
 
 namespace OkSaturate.ViewModels;
 
 /// <summary> MainWindow.xaml 的视图模型 </summary>
 internal partial class MainViewModel : ObservableObject
 {
+    #region 添加和移除待处理图像
 
+    /// <summary> 待处理图像的路径 </summary>
+    public ObservableCollection<string> ImagePaths { get; } = [];
+
+    /// <summary> 选中的待处理图像路径的索引 </summary>
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(RemovePathCommand))]
+    private int _selectedImagePathIndex = -1;
+
+    /// <returns> 是否选中了待处理图像路径 </returns>
+    private bool HasSelectedPath
+        => SelectedImagePathIndex >= 0
+        && SelectedImagePathIndex < ImagePaths.Count;
+
+    /// <summary> 添加待处理图像路径 </summary>
+    [RelayCommand]
+    private void AddPath() => Try.Do("添加图像", () =>
+    {
+        OpenFileDialog dialog = new()
+        {
+            Title = "选取待处理的图像",
+            Filter = "图像文件 (*.bmp;*.jpg;*.jpeg;*.png;*.tif;*.tiff;*.webp)|"
+                   + "*.bmp;*.jpg;*.jpeg;*.png;*.tif;*.tiff;*.webp|"
+                   + "所有文件 (*.*)|*.*",
+            Multiselect = true
+        };
+        if (dialog.ShowDialog() == true)
+            foreach (string path in dialog.FileNames)
+                if (!ImagePaths.Contains(path))
+                    ImagePaths.Add(path);
+    });
+
+    /// <summary> 移除选中的待处理图像路径 </summary>
+    [RelayCommand(CanExecute = nameof(HasSelectedPath))]
+    private void RemovePath() => Try.Do("移除图像", () =>
+    {
+        ImagePaths.RemoveAt(SelectedImagePathIndex);
+        SelectedImagePathIndex = -1; // 避免连续移除
+    });
+
+    #endregion
 }
