179aed938881de7268cdd4d692952e66410b429f
Garth天卜 <g-art-h@outlook.com>
2025-08-08 02:34:46 +0800

    添加拖拽处理 大体完工，进入debug阶段

--- OkSaturate/Views/MainWindow.xaml.cs
+++ OkSaturate/Views/MainWindow.xaml.cs
@@ -1,4 +1,22 @@
+using OkSaturate.ViewModels;
+using System.Windows;
+
 namespace OkSaturate.Views;
 
-public partial class MainWindow : System.Windows.Window
-{ public MainWindow() => InitializeComponent(); }
+public partial class MainWindow : Window
+{
+    public MainWindow() => InitializeComponent();
+
+    /// <summary> 向ListBox中拖拽待处理的图像 </summary>
+    private void ListBox_Drop(object sender, DragEventArgs e)
+    {
+        if (DataContext is MainViewModel vm
+            && e.Data.GetData(DataFormats.FileDrop) is string[] paths)
+        {
+            foreach (var path in paths)
+                if (!vm.ImagePaths.Contains(path))
+                    vm.ImagePaths.Add(path);
+            vm.ProcessCommand.NotifyCanExecuteChanged();
+        }
+    }
+}
